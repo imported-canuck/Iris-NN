126b85c42c50740e0b7a780c1253acc1b90e73e4
C. A. <146338867+imported-canuck@users.noreply.github.com>
2025-04-30 10:22:36 -0400

    Loads data
    Loads data from Iris dataset via scikit-learn. Sets random state for reproducability and invokes 80-20 split to create labelled train and test set. Does not standardize features.

---
+++ data_loader.py
@@ -0,0 +1,14 @@
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+import numpy as np
+
+def load_data(test_size=0.2, random_state=1): # Train-test split is 80-20
+    iris = load_iris()
+    X = iris.data.astype(np.float32)      # Design matrix (150, 4)
+    # Each of the 4 columns is a feature of the data point. 
+    y = iris.target                       # Labels (150,) 1D array
+    # Shuffle & split
+    return train_test_split(X, y,
+                            test_size=test_size,
+                            random_state=random_state,
+                            stratify=y)   # stratify to preserve class distribution

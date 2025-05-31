0b477c31ef174af4f64e01262f643ecedfa6d7f1
imported-canuck <146338867+imported-canuck@users.noreply.github.com>
2025-05-31 01:03:59 -0400

    Same NN to work on digits dataset

--- data_loader.py
+++ data_loader.py
@@ -1,14 +1,14 @@
 # Load the iris dataset and split it into training and testing sets.
 
-from sklearn.datasets import load_iris
+from sklearn.datasets import load_digits
 from sklearn.model_selection import train_test_split
 import numpy as np
 
 def load_data(test_size=0.2, random_state=1): # Train-test split is 80-20
-    iris = load_iris()
-    X = iris.data.astype(np.float32)      # Design matrix (150, 4)
+    digits = load_digits()
+    X = digits.data.astype(np.float32) / 16.0 # Normalize the data to [0, 1] range
     # Each of the 4 columns is a feature of the data point. 
-    y = iris.target                       # Labels (150,) 1D array
+    y = digits.target                         # Labels (150,) 1D array
     # Shuffle & split
     return train_test_split(X, y,
                             test_size=test_size,
--- main.py
+++ main.py
@@ -4,9 +4,9 @@ from trainer import Trainer
 
 def main():
     # Hyperparameters
-    layers = [4, 16, 16, 3]    # Neural network architecture: 4 input features, 2 hidden layers with 16 neurons each, and 3 output classes
-    epochs = 100               # Number of epochs to train
-    batch  = 16                # Batch size for mini-batch gradient descent
+    layers = [64, 32, 32, 10]  # Neural network architecture: 64 input features (pixels), 2 hidden layers with 32 neurons each, and 10 output classes
+    epochs = 500               # Number of epochs to train
+    batch  = 32                # Batch size for mini-batch gradient descent
     lr     = 0.05              # Learning rate for weight updates
 
     # Load Iris
